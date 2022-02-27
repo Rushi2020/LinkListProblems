aa0186a4463d6bc909367b88215c9beda0384be4
Rushi <189489rishi@gmail.com>
2022-02-27 22:22:29 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LinkListProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkListOperation listOperation = new LinkListOperation();
+            listOperation.insert(56);
+            listOperation.insert(30);
+            listOperation.insert(70);
+            listOperation.display();
+        }
+    }
+}
